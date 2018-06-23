43b4492f8ac4ce990453a131dbb248fe05ea8df2
Minsun Lee <dalzony@gmail.com>
2018-06-23 20:13:03 +0900

    setUp 호출하기

--- xunit.py
+++ xunit.py
@@ -3,8 +3,12 @@ class TestCase:
         self.name = name
 
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+
+    def setUp(self):
+        pass
 
 class WasRun(TestCase):
     def __init__(self,name):
@@ -14,21 +18,24 @@ class WasRun(TestCase):
     def testMethod(self):
         self.wasRun = 1
 
-    #def run(self):
-    #    method = getattr(self, self.name)
-    #    method()
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
+
     def testRunning(self):
         test = WasRun("testMethod")
-        assert(not test.wasRun)
+        #assert(not test.wasRun) setUp으로 인해 지워짐
         test.run()
         assert(test.wasRun)
 
-
-#test = WasRun("testMethod")
-#print test.wasRun
-#test.run()
-#print test.wasRun
-
+TestCaseTest("testSetUp").run()
 TestCaseTest("testRunning").run()
