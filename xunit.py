1264e745439bb89bcf62390e557e0bcbd66028fa
Minsun Lee <dalzony@gmail.com>
2018-06-23 19:59:36 +0900

    테스트 메서드 호출하기 V

---
+++ xunit.py
@@ -0,0 +1,34 @@
+class TestCase:
+    def __init__(self,name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
+    def __init__(self,name):
+        self.wasRun = None
+        TestCase.__init__(self,name)
+
+    def testMethod(self):
+        self.wasRun = 1
+
+    #def run(self):
+    #    method = getattr(self, self.name)
+    #    method()
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+
+#test = WasRun("testMethod")
+#print test.wasRun
+#test.run()
+#print test.wasRun
+
+TestCaseTest("testRunning").run()
